ba716a24ed9cc3397e24a45f9eadec0be2c38a2b
Sina Ataei <sinaataei@outlook.com>
2022-02-04 13:21:28 +0300

    showing “under construction” page

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +25,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,11 +36,13 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseFileServer();
+            app.UseFileServer(new FileServerOptions { EnableDefaultFiles = true});
+
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"Hosting environment: {env.EnvironmentName}");
+                await context.Response.WriteAsync($"Hello world!");
             });
         }
     }
