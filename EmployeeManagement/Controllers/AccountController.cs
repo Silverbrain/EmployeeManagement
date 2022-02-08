e0ffa84610beb0f3cc8da3cda528ec0d4d9cfa6f
Sina Ataei <sinaataei@outlook.com>
2022-02-08 21:39:54 +0300

    fix open redirect vulnerability

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -63,7 +63,7 @@ namespace EmployeeManagement.Controllers
                 
                 if(result.Succeeded)
                 {
-                    if(!string.IsNullOrEmpty(returnUrl))
+                    if(!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
                     {
                         return Redirect(returnUrl);
                     }
