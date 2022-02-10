9bcf606ecb043318eeb0eb69d4353e19a703b354
Sina Ataei <sinaataei@outlook.com>
2022-02-10 19:30:50 +0300

    Administration controller added

---
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var role = new IdentityRole(model.Name);
+                var result = await roleManager.CreateAsync(role);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ EmployeeManagement/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
